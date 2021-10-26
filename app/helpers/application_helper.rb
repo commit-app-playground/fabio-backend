011fc1ca2a9cac2e0c2b73db7fac69d22959cd3e
Fabio Miranda <fabio@miranti.net.br>
2021-10-26 15:56:15 -0300

    Add body helper to generate proper ID/classes CSS/JS namespaces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def body(&block)
+    base = ([params[:controller], action_name] + request.variant).join '/'
+    content_tag(:body, id: base.tr('/', '_'), class: base.tr('/', ' '), &block)
+  end
 end
